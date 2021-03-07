9fcaa7bf98cd4fd839489c5ed4be6dc894eaf74a
AnyaNebuko <annanebuko@gmail.com>
2021-03-07 10:04:17 +0300

    Two question quiz

---
+++ Quiz/app/src/main/java/com/nebuko/quiz/QuizDbHelper.java
@@ -0,0 +1,78 @@
+package com.nebuko.quiz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.nebuko.quiz.QuizContract.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "MyAwesomeQuiz.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private SQLiteDatabase db;
+
+    public QuizDbHelper(Context context) {
+        super(context, DATABASE_NAME , null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuestionTable.TABLE_NAME + " ( " +
+                QuestionTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionTable.COLUMN_QUESTION + " TEXT, " +
+                QuestionTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionTable.COLUMN_OPTION3 + " TEXT, " +
+                QuestionTable.COLUMN_ANSWER_NR + " INTEGER" +
+                ")";
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionsTable ();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + QuestionTable.TABLE_NAME);
+        onCreate(db);
+    }
+    private void fillQuestionsTable(){
+        Question q1 = new Question("What does NULL mean in databases?", " No value", "Null", "Empty value", 1);
+        addQuestion(q1);
+        Question q2 = new Question("What is responsible for data security in a DB?", "User", "Database management system", "Database", 2);
+        addQuestion(q2);
+    }
+    private void addQuestion (Question question){
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuestionTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuestionTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuestionTable.COLUMN_OPTION3, question.getOption3());
+        cv.put(QuestionTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        db.insert(QuestionTable.TABLE_NAME, null, cv);
+    }
+    public ArrayList<Question> getAllQuestions(){
+        ArrayList<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("Select * FROM " + QuestionTable.TABLE_NAME, null);
+        if (c.moveToFirst()){
+            do{
+                Question question = new Question();
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION3)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionTable.COLUMN_ANSWER_NR)));
+                questionList.add(question);
+            }while (c.moveToNext());
+        }
+        c.close();
+        return questionList;
+    }
+}
